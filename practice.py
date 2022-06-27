a9f8d60bb299d29db8afce6a3f30242a77e11b20
tigerstonebs <108073222+tigerstonebs@users.noreply.github.com>
2022-06-27 19:39:30 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,1 +1,31 @@
 print("hello World")
+print("Mary's cosmetics")
+print('신씨가 소리질렀다."도둑이야".')
+print('"C:\Windows"')
+print('안녕하세요.\n만나서\t\t반갑습니다.')
+print('오늘은','일요일')
+print("naver", "kakao", "samsung", sep=';')
+print("naver", "kakao", "sk", "samsung", sep="/")
+print("first", end="");print("second")
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+s= "hello"
+t= "python"
+print(s+'!', t)
+
+num_str = "720"
+num_int = int(num_str)
+print(num_int+1, type(num_int))
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+
+
+
+
+
+
+print('3')
+
