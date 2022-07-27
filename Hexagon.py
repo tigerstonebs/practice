d11c54a20bd8d2eeb2450406f6de90e479c537f4
tigerstonebs <108073222+tigerstonebs@users.noreply.github.com>
2022-07-27 15:36:39 +0900

    Add files via upload

---
+++ Hexagon.py
@@ -0,0 +1,15 @@
+import turtle
+myTurtle = turtle.Turtle()
+myTurtle.forward(100)
+myTurtle.right(60)
+myTurtle.forward(100)
+myTurtle.right(60)
+myTurtle.forward(100)
+myTurtle.right(60)
+myTurtle.forward(100)
+myTurtle.right(60)
+myTurtle.forward(100)
+myTurtle.right(60)
+myTurtle.forward(100)
+
+turtle.done()
