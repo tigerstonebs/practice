a6a6b51a3d7ea9cdf73c2c5ea74d8866bccc118e
tigerstonebs <108073222+tigerstonebs@users.noreply.github.com>
2022-08-02 23:45:01 +0900

    Add files via upload

---
+++ baekjoon practice.py
@@ -0,0 +1,25 @@
+# a = int(input())
+# if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0:
+#     print(1)
+# else:
+#     print(0)
+# a = int(input())
+# b = int(input())
+
+# if a > 0 and b > 0:
+#     print(1)
+# elif a < 0 and b > 0:
+#     print(2)
+# elif a < 0 and b < 0:
+#     print(3)
+# elif a > 0 and b < 0:
+#     print(4)
+# H, M = map(int, input().split())
+# if M > 44:
+#     print(H, M-45)
+# elif M < 45 and H > 0:
+#     print(H-1, M+15)
+# else:
+#     print(23, M+15)
+list = ["acsca", "scaasca"]
+print(list[0][1])
